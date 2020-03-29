7e688ffc7a0ab47e8a25c323166a717b1175c2b5
hbFree <ih_beldjoudi@esi.dz>
2020-03-29 21:57:12 +0100

    implement feed_forward

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,11 +1,12 @@
 import numpy as np
+from sklearn.neural_network import MLPClassifier
 
 
 class NeuralNet:
-    def __init__(self, type='reg', hidden_layer_sizes=(5,), learning_rate=1e-3, regularization_rate=1e-4,
+    def __init__(self, nn_type='reg', hidden_layer_sizes=(5,), learning_rate=1e-3, regularization_rate=1e-4,
                  activation='relu', tol=1e-3, max_iter=300):
         self.hidden_layer_size = hidden_layer_sizes
-        self.type = type
+        self.nn_type = nn_type
         self.learning_rate = learning_rate
         self.regularization_rate = regularization_rate
         self.activation = activation
@@ -14,7 +15,45 @@ class NeuralNet:
         self.weighs = []
         self.fitted = 0
 
+    def fit(self, X, y, verbose=True):
+        self.fitted = True
+        m, n = X.shape
+        out_size = 1
+        if y.ndim == 2:
+            out_size = y.shape[1]
+        self.init_weighs(input_size=n, output_size=out_size)
+
+    def predict(self, X, prob=0):
+        self.check_for_error()
+        if prob:
+            return self.feed_forward(X, return_history=0).T
+
+    def score(self, X, y):
+        self.check_for_error()
+
+    def feed_forward(self, X, return_history=0):
+        # returns the list of layers after feed forward
+        self.check_for_error()
+        y_p = X.T
+        history = []
+        for i, w in enumerate(self.weighs):
+            y_p = np.dot(w, np.insert(y_p, [0], 1, axis=0))
+            if return_history and i < (len(self.weighs) - 1):
+                y_p = self.activate(y_p)
+                history.append(y_p)
+        if 'c' in self.nn_type:  # classifier
+            if y_p.ndim == 1:
+                y_p = self.sig(y_p)
+            else:
+                y_p = np.exp(y_p)
+                y_p /= np.sum(y_p, axis=0)
+        if return_history:
+            history.append(y_p)
+            return history
+        return y_p
+
     def init_weighs(self, input_size, output_size):
+        # Xavier init weighs if 'tanh' activation and He if 'ReLu'
         layer_sizes = [input_size] + list(self.hidden_layer_size) + [output_size]
         n_layer = len(layer_sizes)
         # special weighs init
@@ -27,27 +66,14 @@ class NeuralNet:
             n_cols = layer_sizes[i] + 1
             self.weighs.append(np.random.randn(n_rows, n_cols) * cf(n_cols))
 
-    def fit(self, X, y, verbose=True):
-        m, n = X.shape
-        self.init_weighs(input_size=n, output_size=len(y))
-
-    def predict(self, X, prob=0):
-        self.check_for_error()
-
-    def score(self, X, y):
-        self.check_for_error()
-
-    def feed_forward(self, X, weighs):
-        pass
-
-    def activate(self, layer):
-        f = getattr(self, self.activation)
-        return f(layer)
-
     def check_for_error(self):
         # predict the output for the matrix of inputs
         if not self.fitted:
             raise RuntimeError('Model not fitted yet!')
+
+    def activate(self, layer):
+        f = getattr(self, self.activation)
+        return f(layer)
 
     @staticmethod
     def sig(matrix):
@@ -62,3 +88,10 @@ class NeuralNet:
     def tanh(matrix):
         tmp = np.exp(-2 * matrix)
         return (1 - tmp) / (1 + tmp)
+
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0, 1], [1, 1], [1, 2], [0, 3]])
+
+model = NeuralNet(nn_type='classifier', hidden_layer_sizes=(3, 2), regularization_rate=0, activation='sig')
+model.fit(X, y)
