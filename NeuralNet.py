f719467ec0edd96049d4857c815ec677bf905c78
hbFree <ih_beldjoudi@esi.dz>
2020-03-30 20:38:31 +0100

    implement NeuralNet's fit with Batch Gradient Deescent

--- NeuralNet.py
+++ NeuralNet.py
@@ -26,14 +26,28 @@ class NeuralNet:
         if y.ndim == 2:
             out_size = y.shape[1]
         self.init_weighs(input_size=n, output_size=out_size)
-        print(self.weighs)
-        y_p = self.feed_forward(X, return_history=1)
-        print(y_p)
+        error_calculator = self.avg_squared_error
+        if 'c' in self.nn_type:
+            error_calculator = self.avg_cross_entropy
+        for k in range(max_iter):
+            history = [X.T] + self.feed_forward(X, return_history=1)
+            y_p = history[-1]
+            if verbose:
+                print(f'iteration {k}, {error_calculator.__name__} = {error_calculator(y, y_p, reg)}')
+            n_lay = len(history)
+            for i in range(n_lay - 1):
+                act_i = np.insert(history[i], [0], 1, axis=0)  # add bias line (1)
+                sigma_nxt = history[i + 1] - y_p[0]
+                grad = np.dot(sigma_nxt, act_i.T) / m
+                grad[:, 1:] += (regularization_rate * (self.weighs[i])[:, 1:]) / m
+                self.weighs[i] -= (learning_rate * grad)
 
-    def predict(self, X, prob=0):
+    def predict(self, X, prob=0, thresh=0.5):
         self.check_for_error()
+        y = self.feed_forward(X, return_history=0)[0].T
         if prob:
-            return self.feed_forward(X, return_history=0).T
+            return y
+        return np.int32(y >= thresh)
 
     def score(self, X, y):
         self.check_for_error()
@@ -45,15 +59,13 @@ class NeuralNet:
         history = []
         for i, w in enumerate(self.weighs):
             y_p = np.dot(w, np.insert(y_p, [0], 1, axis=0))
-            if return_history and i < (len(self.weighs) - 1):
+            if i < (len(self.weighs) - 1):
                 y_p = self.activate(y_p)
-                history.append(y_p)
+                if return_history:
+                    history.append(y_p)
         if 'c' in self.nn_type:  # classifier
             if y_p.shape[0] == 1:
-                y_p = self.sig(y_p)[0]
-            else:
-                y_p = np.exp(y_p)
-                y_p /= np.sum(y_p, axis=0)
+                y_p = self.sig(y_p)
         if return_history:
             history.append(y_p)
             return history
@@ -71,13 +83,13 @@ class NeuralNet:
         for i in range(n_layer - 1):
             n_rows = layer_sizes[i + 1]
             n_cols = layer_sizes[i] + 1
-            w = np.random.randn(n_rows, n_cols-1) * cf(n_cols-1)
+            w = np.random.randn(n_rows, n_cols - 1) * cf(n_cols - 1)
             w = np.insert(w, [0], 1, axis=1)
             self.weighs.append(w)
 
     def avg_cross_entropy(self, y, y_p, reg='l2'):
         # compute cross entropy error with regularization rate (l1 or l2)
-        ce = -np.sum((y * np.log(y_p) + (1 - y) * np.log(1 - y_p)))  # average cross entropy
+        ce = -np.sum(y * np.log(y_p)) - np.sum((1 - y) * np.log(1 - y_p))  # average cross entropy
         regularizator = self.compute_regularization_rate(reg)
         return (ce + regularizator) / len(y)
 
@@ -92,7 +104,7 @@ class NeuralNet:
             for w in self.weighs:
                 regularizator += np.sum(w[:, 1:] ** 2)
             regularizator *= (self.regularization_rate / 2)
-        else:
+        else:  # l1
             for w in self.weighs:
                 regularizator += np.sum(np.abs(w[:, 1:]))
             regularizator *= self.regularization_rate
@@ -125,5 +137,7 @@ class NeuralNet:
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, 1, 1, 0])
 
-model = NeuralNet(nn_type='classifier', hidden_layer_sizes=(3, 2), activation='sig')
-model.fit(X, y, learning_rate=0.001, regularization_rate=0, verbose=1)
+model = NeuralNet(nn_type='class', hidden_layer_sizes=(1, 1), activation='sig')
+model.fit(X, y, learning_rate=0.0001, regularization_rate=0, verbose=1, max_iter=500000)
+
+print(model.predict(X, prob=1))
